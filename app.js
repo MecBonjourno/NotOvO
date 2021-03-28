fd26d13d861b4d9b88d76247f91428186c421fa3
Guilherme Correa Zago <guilherme.zago@pampa.compasso>
2021-03-28 04:53:36 -0300

    make it better

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 require('dotenv').config();
 
-const PORT = 5000
+const PORT = process.env.PORT ||5000
 const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
